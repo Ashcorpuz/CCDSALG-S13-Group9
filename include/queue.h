5a0c4601e49c1229decf446d208b3b586350f06f
Ashcorpuz <137604451+Ashcorpuz@users.noreply.github.com>
2024-05-29 19:58:48 +0800

    Add files via upload

---
+++ include/queue.h
@@ -0,0 +1,59 @@
+/**
+ * queue.h
+ * Author: Ashley Corpuz, Princess Escobar, Herise Visto
+ *
+ * Using 'ADT' design pattern
+ * 	referenced from: https://leanpub.com/patternsinc
+ * Queue has limitations, after a bit of enqueuing and dequeuing,
+ * the size of queue is reduced.
+ * That's why this queue is modified to Circular Queue.
+ *
+ * Opaque Data Type
+ *
+ * Circular Queue reference: https://www.programiz.com/dsa/queue
+ * Queue Library Header
+ */
+
+#ifndef __QUEUE_H_INCLUDED
+#define __QUEUE_H_INCLUDED
+
+#define bool _Bool
+#define STRING_SIZE 8
+/* Pointer to Incomplete Type. */
+typedef struct Queue *QueuePtr;
+typedef struct _Kyu *KyuPtr;
+
+struct Queue
+{
+	// Add new Item to last in queue
+	void (*enqueue)(QueuePtr self, char *item);
+
+	// Remove the first item in queue
+	char *(*dequeue)(QueuePtr self);
+
+	// Peek at the first item in queue
+	char *(*peek)(QueuePtr self);
+
+	// Check if full
+	bool (*isFull)(QueuePtr self);
+
+	// Check if empty
+	bool (*isEmpty)(QueuePtr self);
+
+	// Get size of queue
+	unsigned int (*size)(QueuePtr self);
+
+	// The Queue
+	KyuPtr _kyu;
+};
+
+// FUNCTIONS Prototypes:
+/**
+ * Initialize Queue operations and implement queue
+ * @param name the name of queue
+ * @param capacity the maximum size of queue
+ * @return QueuePtr
+ */
+QueuePtr initQueue(unsigned int capacity);
+
+#endif // __QUEUE_H_INCLUDED
---
+++ include/stack.h
@@ -0,0 +1,34 @@
+/**
+ * stack.h
+ * Author: Ashley Corpuz, Princess Escobar, Herise Visto
+ *
+ *  Using 'ADT' design pattern
+ * 		referenced from: https://leanpub.com/patternsinc
+ *
+ *  Stack Library Header
+ */
+
+#ifndef __STACK_H_INCLUDED
+#define __STACK_H_INCLUDED
+
+#define bool _Bool
+#define STRING_SIZE 8
+
+typedef struct Stack *StackPtr;
+typedef struct _Pile *PilePtr;
+
+struct Stack
+{
+	void (*push)(StackPtr self, char *item);
+	char *(*pop)(StackPtr self);
+	char *(*top)(StackPtr self);
+	bool (*isFull)(StackPtr self);
+	bool (*isEmpty)(StackPtr self);
+	unsigned int (*size)(StackPtr self);
+	// The Stack
+	PilePtr _pile;
+};
+
+StackPtr initStack(unsigned int capacity);
+
+#endif // __STACK_H_INCLUDED
