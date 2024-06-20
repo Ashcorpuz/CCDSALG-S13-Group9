96aa30dec7a52971d19dfebb4b6d2764cb10ef33
Princess Escobar <70127731+itzesco@users.noreply.github.com>
2024-06-20 11:58:44 +0800

    Add files via upload

--- MCO1-main/include/main.h
+++ MCO1-main/include/main.h
@@ -7,6 +7,7 @@
 #define MAX 256
 
 void infixToPostfix(const char *infix, QueuePtr result);
+int evaluatePostfix(char *postfix, char *error);
 char *duplicate(const char *str);
 
-#endif //__MAIN_H_INCLUDED
+#endif // __MAIN_H_INCLUDED
--- MCO1-main/src/main.c
+++ MCO1-main/src/main.c
@@ -1,4 +1,5 @@
 #include "../include/main.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
@@ -9,7 +10,9 @@ int main()
 {
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     QueuePtr postfix = createQueue(MAX);
+    QueuePtr converted = createQueue(MAX);
     char infix[MAX];
+    char error[50] = "";
     /*
     Sample
     Infix: 8*(5^4+2)-6^2/(9*3);
@@ -29,17 +32,25 @@ int main()
             return 0;
         }
         else{
+            infixToPostfix(infix,postfix);
+            converted = postfix;
             printf("\nEvaluation: ");
             SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
             /* Insert Code for Evaluation*/
-
+            printf("\nEvaluation: ");
+            int result = evaluatePostfix(postfix, error);
+            if (strlen(error) > 0) {
+                printf("%s", error);
+            } else {
+                printf("%d", result);
+            }
             printf(ANSI_COLOR_RESET); // resets text color
             printf("\nPostfix: ");
+            
             SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN);
-            infixToPostfix(infix,postfix);
-            while (!isEmptyQueue(postfix))
+            while (!isEmptyQueue(converted))
             {
-                printf("%s", dequeue(postfix));
+                printf("%s", dequeue(converted));
             }
             printf("\n\n");
             free(postfix);
